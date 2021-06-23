60853cae8e7bbcd98bba0cbd231958ae8975ade8
Nawabmeeravali <meeravalinawab@gmail.com>
2021-06-23 20:26:59 +0530

    fair : fix redeclaration

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -8172,12 +8172,11 @@ static int find_energy_efficient_cpu(struct sched_domain *sd,
 	int placement_boost = task_boost_policy(p);
 	u64 start_t = 0;
 	int next_cpu = -1, backup_cpu = -1;
-	int boosted = (schedtune_task_boost(p) > 0 || per_task_boost(p) > 0);
 
 #ifdef CONFIG_SCHED_TUNE
-	int boosted = (schedtune_task_boost(p) > 0);
+	int boosted = (schedtune_task_boost(p) > 0 || per_task_boost(p) > 0);
 #elif  CONFIG_UCLAMP_TASK
-	int boosted = (uclamp_boosted(p) > 0);
+	int boosted = (uclamp_boosted(p) > 0 || per_task_boost(p) > 0);
 #endif
 	fbt_env.fastpath = 0;
 	fbt_env.need_idle = 0;
